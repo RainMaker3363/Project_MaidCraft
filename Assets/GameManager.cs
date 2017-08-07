dc31b2ab21ac0d2c35a811b4c65601bc68ab507e
rainmaker3363 <jsc22637@gmail.com>
2017-08-07 15:19:25 +0900

    #Summer Sync 1-1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameOption
+{
+    enum GameControllOption
+    {
+        GAMEPAD  = 0,
+        KEYBOARD = 1,
+        MAX
+    }
+}
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // 바닥 충돌 용
+    private SphereCollider Sphere_col;
+    private Vector3 BackUpPos;
+
+	// Use this for initialization
+	void Start () {
+		if(Sphere_col == null)
+        {
+            Sphere_col = GameObject.FindGameObjectWithTag("PlayerCollider").GetComponent<SphereCollider>();
+            Debug.Log("Collider Find!");
+        }
+        else
+        {
+            Debug.Log("Collider Find Failed!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
